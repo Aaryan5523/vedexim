030bc499730928fd4df8ee2199f69ce4ddf0749c
Aaryan <aaryanvidja31@gmail.com>
2026-08-19 14:27:33 +0530

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 import Navbar from "./components/Navbar";
 import WhatsAppButton from "./components/WhatsAppButton";
